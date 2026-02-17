ce10225dd75da69a3e1e41a0d28884b5dea78b02
TopScrech <sergei_saliukov@icloud.com>
2026-02-17 18:29:56 +0100

    Added DeviceKit to display a better device name

--- PeerChat/ValueStore.swift
+++ PeerChat/ValueStore.swift
@@ -1,6 +1,7 @@
 import SwiftUI
+import DeviceKit
 
 final class ValueStore: ObservableObject {
-    @AppStorage("nickname") var nickname = UIDevice.current.model
+    @AppStorage("nickname") var nickname = Device.current.name ?? UIDevice.current.model
     @AppStorage("status") var status = ""
 }
