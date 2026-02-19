be41123d08b164509cce83c4bc2cb192cc1f60de
TopScrech <sergei_saliukov@icloud.com>
2026-02-19 12:14:09 +0100

    Remove TabView and profile tab

--- PeerChat/HomeView.swift
+++ PeerChat/HomeView.swift
@@ -11,21 +11,9 @@ struct HomeView: View {
     }
     
     var body: some View {
-        TabView {
-            ContentView()
-                .tag("Chats")
-                .tabItem {
-                    Label("Chats", systemImage: "ellipsis.bubble")
-                }
-            
-            ProfileView()
-                .tag("Profile")
-                .tabItem {
-                    Label("Profile", systemImage: "person.crop.circle")
-                }
-        }
-        .environment(model)
-        .environment(crypto)
+        ContentView()
+            .environment(model)
+            .environment(crypto)
     }
 }
 
